431251b3c5b1d04175f844d7bb3b7977ee60b507
Abdul Khan <abdulkhan333c@gmail.com>
2025-09-06 11:07:07 +0530

    Added Code

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1" # Change to your region
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (update for your region)
+  instance_type = "t2.micro"
+  key_name      = "MyKeyPair"             # Replace with your key pair name
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
+}
