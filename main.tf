bae74566b6cd8e278f476eecccbb658bb8d62a7f
Abdul khan <abdulkhan333c@gmail.com>
2025-09-06 11:10:26 +0530

    Update main.tf
    Remove unwanted use

--- main.tf
+++ main.tf
@@ -1,13 +1,12 @@
 provider "aws" {
-  region = "us-east-1" # Change to your region
+  region = "us-east-1" 
 }
 
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (update for your region)
+  ami           = "ami-0c55b159cbfafe1f0" 
   instance_type = "t2.micro"
-  key_name      = "MyKeyPair"             # Replace with your key pair name
-
+  key_name      = "MyKeyPair"            
   tags = {
-    Name = "MyEC2Instance"
+    Name = "DEV"
   }
 }
